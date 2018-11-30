609bdb8b2753a949d8acd80dfeffcede6319562b
Alex Ko <ko.e.alexander@gmail.com>
2018-11-30 09:22:26 -0500

    Working with objects in kotlin - singleton

---
+++ app/src/main/java/com/example/koeal/coderswag/Category.kt
@@ -0,0 +1,5 @@
+package com.example.koeal.coderswag
+
+class Category(val title: String, val image: String) {
+
+}
---
+++ app/src/main/java/com/example/koeal/coderswag/Product.kt
@@ -0,0 +1,5 @@
+package com.example.koeal.coderswag
+
+class Product(val title: String, val price: String, val image: String) {
+
+}
